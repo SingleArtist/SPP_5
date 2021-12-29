cca9980ab70bef21e0f7b632ee92048178b6fd3c
SingleArtist <95147229+SingleArtist@users.noreply.github.com>
2021-12-29 20:35:39 +0300

    Add files via upload

---
+++ MPP5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+//Задание 5
+/*Реализовать консольную программу на языке C#, которая:
+- принимает в параметре командной строки путь к сборке .NET
+(EXE- или DLL-файлу);
+-загружает указанную сборку в память;
+-выводит на экран полные имена всех public -типов данных этой
+сборки, упорядоченные по пространству имен (namespace) и по
+имени.*/
+
+namespace FifthLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.LoadFrom("D:\\UNIVERSITY\\osisp2\\MPP_lab1.dll");
+
+            var types = assembly.GetTypes().Where(t => t.IsPublic).OrderBy(t => t.Namespace + t.Name);
+            foreach (var type in types)
+            {
+                Console.Out.WriteLine(type.FullName);
+
+            }
+        }
+    }
+}
+
